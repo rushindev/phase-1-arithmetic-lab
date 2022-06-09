07c8fa4b115197152eb6a13a754d611c44aeedda
York Rushin <rushinyork@gmail.com>
2022-06-08 21:43:11 -0700

    commit tree dd2ba8b83f283c4f95dc80254fa915bbd669fa67

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let multiply;
+let num1 = 2;
+let num2 = 31;
+let random;
+let mod;
+let max;
+let num3 = 19;
+let num4 = 5;
+multiply = num1 * num2;
+random = Math.floor(Math.random()) + 1;
+mod = num3 % num4;
+max = Math.max(5, 10, 15, 20);
